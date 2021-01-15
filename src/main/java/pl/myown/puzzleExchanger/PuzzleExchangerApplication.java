a910db213708c3c128660646cfbf2d12c2519051
Araggorn <zerelik@gmail.com>
2021-01-15 21:05:04 +0100

    initial commit

---
+++ src/main/java/pl/myown/puzzleExchanger/PuzzleExchangerApplication.java
@@ -0,0 +1,13 @@
+package pl.myown.puzzleexchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleExchangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuzzleExchangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/myown/puzzleexchanger/PuzzleexchangerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.myown.puzzleexchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzleexchangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
