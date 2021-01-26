7a1456d3b72d656d5df577aa07d52cff9e130f86
Araggorn <zerelik@gmail.com>
2021-01-26 13:00:56 +0100

    some addings

---
+++ src/main/java/pl/myown/puzzleExchanger/entity/Contact.java
@@ -0,0 +1,28 @@
+package pl.myown.puzzleexchanger.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name="contact")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String message;
+
+}
---
+++ src/main/java/pl/myown/puzzleExchanger/repository/ContactRepository.java
@@ -0,0 +1,9 @@
+package pl.myown.puzzleexchanger.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.myown.puzzleexchanger.entity.Contact;
+
+@Repository
+public interface ContactRepository extends JpaRepository <Contact, Long> {
+}
